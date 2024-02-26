d17ab8470424e744423f2e65af297f029d89fe7f
Maikon Rodrigues <maikorodrigues787@gmail.com>
2024-02-25 21:47:05 -0300

    primeiro commit

---
+++ agendame/src/test/java/com/example/agendame/AgendameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
